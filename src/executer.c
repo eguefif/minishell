01a7b3bd9c8649bbaa91c6aa5ea66e6725120e26
Emmanuel Guefif <eguefif@c1r3p8.42quebec.com>
2023-11-20 14:11:22 -0500

    Before branch

--- src/executer.c
+++ src/executer.c
@@ -6,7 +6,7 @@
 /*   By: eguefif <eguefif@student.42quebec.>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 12:16:43 by eguefif           #+#    #+#             */
-/*   Updated: 2023/11/20 13:24:57 by eguefif          ###   ########.fr       */
+/*   Updated: 2023/11/20 14:10:52 by eguefif          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,10 @@ static void handle_pipes(t_command *commands, char **env);
 int	ms_execute(t_command *commands, char **env)
 {
 	int	retval;
-	int	stat_loc;
 
 	retval = fork();
 	if (!retval)
 		handle_pipes(commands, env);
-	while (1)
-		sleep(1);
 	return (0);
 }
 
@@ -33,39 +30,20 @@ static void handle_pipes(t_command *commands, char **env)
 	int	retval;
 	int	stat_loc;
 	int	i;
+	int	stdin_save;
+	int	stdout_save;
 
 	i = 0;
+	stdin_save = dup(0);
+	stdout_save = dup(1);
 	while (!commands[i].last)
 	{
-		if (commands[i + 1].last)
-		{
-			retval = fork();
-			if (!retval)
-			{
-				if (execve(commands[i].args[0], commands[i].args, env) == -1)
-				{
-					ft_dprintf(2, "command %d\n", i);
-					ft_exit(commands);
-				}
-			}
-			else
-			{
-				wait(&stat_loc);
-				exit(0);
-			}
-		}
 		pipe(pipe_fd);
 		retval = fork();
 		if (!retval)
 		{
-			dup2(pipe_fd[0], 0);
-			close(pipe_fd[0]);
-			close(pipe_fd[1]);
-			i++;
-		}
-		else
-		{
-			dup2(pipe_fd[1], 1);
+			if (!commands[i + 1].last)
+				dup2(pipe_fd[1], 1);
 			close(pipe_fd[0]);
 			close(pipe_fd[1]);
 			if (execve(commands[i].args[0], commands[i].args, env) == -1)
@@ -74,5 +52,16 @@ static void handle_pipes(t_command *commands, char **env)
 				ft_exit(commands);
 			}
 		}
+		else
+		{
+			dup2(pipe_fd[0], 0);
+			close(pipe_fd[0]);
+			close(pipe_fd[1]);
+			i++;
+		}
 	}
+	while (waitpid(-1, &stat_loc, 0) > 0)
+		;
+	dup2(stdin_save, 0);
+	dup2(stdout_save, 1);
 }
