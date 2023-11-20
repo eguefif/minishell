d678434c275aebdf30def7e1d11777dc3172479b
Emmanuel Guefif <eguefif@c1r3p8.42quebec.com>
2023-11-20 13:25:06 -0500

    Before branch

--- src/executer.c
+++ src/executer.c
@@ -6,11 +6,13 @@
 /*   By: eguefif <eguefif@student.42quebec.>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 12:16:43 by eguefif           #+#    #+#             */
-/*   Updated: 2023/11/17 13:38:21 by eguefif          ###   ########.fr       */
+/*   Updated: 2023/11/20 13:24:57 by eguefif          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static void handle_pipes(t_command *commands, char **env);
 
 int	ms_execute(t_command *commands, char **env)
 {
@@ -18,13 +20,59 @@ int	ms_execute(t_command *commands, char **env)
 	int	stat_loc;
 
 	retval = fork();
-	if (retval == 0)
+	if (!retval)
+		handle_pipes(commands, env);
+	while (1)
+		sleep(1);
+	return (0);
+}
+
+static void handle_pipes(t_command *commands, char **env)
+{
+	int	pipe_fd[2];
+	int	retval;
+	int	stat_loc;
+	int	i;
+
+	i = 0;
+	while (!commands[i].last)
 	{
-		if (execve(commands[0].args[0], commands[0].args, env) == -1)
+		if (commands[i + 1].last)
 		{
-			exit (1);
+			retval = fork();
+			if (!retval)
+			{
+				if (execve(commands[i].args[0], commands[i].args, env) == -1)
+				{
+					ft_dprintf(2, "command %d\n", i);
+					ft_exit(commands);
+				}
+			}
+			else
+			{
+				wait(&stat_loc);
+				exit(0);
+			}
+		}
+		pipe(pipe_fd);
+		retval = fork();
+		if (!retval)
+		{
+			dup2(pipe_fd[0], 0);
+			close(pipe_fd[0]);
+			close(pipe_fd[1]);
+			i++;
+		}
+		else
+		{
+			dup2(pipe_fd[1], 1);
+			close(pipe_fd[0]);
+			close(pipe_fd[1]);
+			if (execve(commands[i].args[0], commands[i].args, env) == -1)
+			{
+				ft_dprintf(2, "command %d\n", i);
+				ft_exit(commands);
+			}
 		}
 	}
-	waitpid(retval, &stat_loc, 0);
-	return (0);
 }
