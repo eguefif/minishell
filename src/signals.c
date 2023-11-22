d39cb84bb1727275c7b73eddf6d2977be743404c
Maxime Pelletier <maxpelle@c1r7p12.42quebec.com>
2023-11-22 09:45:19 -0500

    Intermediate commit for merging

--- src/signals.c
+++ src/signals.c
@@ -6,14 +6,14 @@
 /*   By: maxpelle <maxpelle@student.42quebec.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 13:54:24 by maxpelle          #+#    #+#             */
-/*   Updated: 2023/11/21 16:14:35 by maxpelle         ###   ########.fr       */
+/*   Updated: 2023/11/22 09:39:26 by maxpelle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static int	signal_init_c(void);
-static void	signal_exec_c(int signal);
+static int	signals_init(void);
+static void	signals_exec(int signal);
 
 int	ms_init_signals(void)
 {
@@ -21,30 +21,36 @@ int	ms_init_signals(void)
 
 	retval = 0;
 	if (!retval)
-		retval = signal_init_c();
+		retval = signals_init();
 	return (retval);
 }
 
-static int	signal_init_c(void)
+static int	signals_init(void)
 {
 	struct sigaction	init_signal_c;
+	struct sigaction	init_ignore;
 
 	sigemptyset(&init_signal_c.sa_mask);
-	init_signal_c.sa_handler = signal_exec_c;
+	init_signal_c.sa_handler = signals_exec;
 	if (sigaction(SIGINT, &init_signal_c, 0) == -1)
 	{
 		ft_error_message("SIGINT", SIGNAL_ERROR);
 		return (1);
 	}
+	init_ignore.sa_handler = SIG_IGN;
+	if (sigaction(SIGQUIT, &init_ignore, 0) == -1)
+	{
+		ft_error_message("SIGQUIT", SIGNAL_ERROR);
+		return (1);
+	}
 	return (0);
 }
 
-static void	signal_exec_c(int signal)
+static void	signals_exec(int signal)
 {
 	int	stat_loc;
 
-	(void) signal;
-	if (waitpid(-1, &stat_loc, WNOHANG) != 0)
+	if (waitpid(-1, &stat_loc, WNOHANG) != 0 && signal == SIGINT)
 	{
 		write(1, "\n", 1);
 		rl_replace_line("", 0);
