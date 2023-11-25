6741b17d2edf0d3ed076a056d1ce2db147a17efb
Emmanuel Guefif <eguefif@c1r3p8.42quebec.com>
2023-11-25 12:14:31 -0500

    NOrme done

--- src/executer_getpath.c
+++ src/executer_getpath.c
@@ -6,7 +6,7 @@
 /*   By: eguefif <eguefif@student.42quebec.>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 18:40:07 by eguefif           #+#    #+#             */
-/*   Updated: 2023/11/24 10:45:52 by eguefif          ###   ########.fr       */
+/*   Updated: 2023/11/25 12:14:13 by eguefif          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,14 @@
 static int	is_relative_path(char *command);
 static int	is_abs_path(char *command);
 static void	handle_error(char *command);
+static char	*check_paths(char *paths, char *command);
 
 char	*get_command_path(char *command, char **env)
 {
-	char 	*path;
+	char	*path;
 	char	**paths;
 	int		i;
 	char	*retval;	
-	char	*tab[3];
 
 	if (is_relative_path(command) || is_abs_path(command))
 		if (access(command, F_OK) == 0)
@@ -31,19 +31,12 @@ char	*get_command_path(char *command, char **env)
 	if (!path)
 		return (0);
 	paths = ft_split(path, ':');
-	i = 0;
-	retval = 0;
-	tab[1] = command;
-	tab[2] = 0;
-	while (paths[i])
+	i = -1;
+	while (paths[++i])
 	{
-		tab[0] = paths[i];
-		retval = ft_strjoin_tab(tab, "/");
-		if (access(retval, F_OK) == 0)
-			break;
-		free(retval);
-		retval = 0;
-		i++;
+		retval = check_paths(paths[i], command);
+		if (retval)
+			break ;
 	}
 	ft_cleansplits(paths);
 	if (retval == 0)
@@ -54,14 +47,14 @@ char	*get_command_path(char *command, char **env)
 
 static int	is_relative_path(char *command)
 {
-	if (command[0]== '.' && command[1] == '/')
+	if (command[0] == '.' && command[1] == '/')
 		return (1);
 	return (0);
 }
 
 static int	is_abs_path(char *command)
 {
-	if (command[0]== '/')
+	if (command[0] == '/')
 		return (1);
 	return (0);
 }
@@ -72,4 +65,21 @@ static void	handle_error(char *command)
 		ft_error_message(command, NO_FILE);
 	else
 		ft_error_message(command, COMMAND_NOT_FOUND);
+}
+
+static char	*check_paths(char *path, char *command)
+{
+	char	*retval;
+	char	*tab[3];
+
+	tab[1] = command;
+	tab[2] = 0;
+	retval = 0;
+	tab[0] = path;
+	retval = ft_strjoin_tab(tab, "/");
+	if (access(retval, F_OK) == 0)
+		return (retval);
+	free(retval);
+	retval = 0;
+	return (0);
 }
